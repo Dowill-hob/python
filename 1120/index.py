32f81fe6711a1269125be8ba18b728a5768118f5
Dowill-hob <wiewk12365@gmail.com>
2024-11-20 20:32:39 +0900

    python

---
+++ 1120/index.py
@@ -0,0 +1,76 @@
+'''
+print("안녕하세요")
+print("Hello","python")
+print("Hello","python",sep="|")
+print("안녕하세요",end="")
+print("저는 김준식입니다.")
+
+print(111111)
+# singer = input("좋아하는 가수는?")
+# print("좋아하는 가수는",singer,"입니다.")
+import keyword
+
+
+# x = 10
+# print("before",x,id(x))
+
+# # y, z = 3.14 , "안녕하세요"
+# # print("y",y)
+# # print("Z",z)
+
+# x = "반갑습니다."
+
+# print("after",x,id(x))
+
+# # x = 10
+
+# # print("x",x)
+
+# a=[1,2,3]
+# print("before a", a, id(a))
+# a.append(4)
+# print("after a", a, id(a))
+
+# print(keyword.kwlist)
+# x = 48/2(9+3)
+# print(x)
+'''
+
+# num = 5
+# num += 5
+# print("+=",num) # 10
+# num -= 2
+# print("-=",num) # 8
+# num *=4
+# print("*=",num) # 32
+# num /= 2
+# print("/=",num) # 16.0
+# num //= 3
+# print("//=",num) # 5.0
+# num %= 3
+# print("%=",num) # 2.0
+# num **= 4
+# print("**=",num) # 16.0
+
+
+# a = 2>1 #true
+# b = 3 < 2 #false
+# c = 1==1 #true
+# d = 3 >=4  #false
+# print(a and c) #true
+# print(a and d) #false
+# print(b or c)  #true
+# print(b or d) #false
+# print(not a) #false
+# print(not d) #true
+
+# a = "hello world"
+# print("H" in a) #False
+# print("h" in a) #true
+
+# print("a" not in a) #true
+# print("w" not in a) #False
+
+num = input("숫자를 입력하세요: ")
+
+print("True면 짝수, False면 홀수 : ", int(num) % 2 == 0)

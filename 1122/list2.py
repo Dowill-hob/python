6fe1085db1a0c94ad58f537892131dbbe48680e7
Dowill-hob <wiewk12365@gmail.com>
2024-11-22 15:02:11 +0900

    1122

---
+++ 1122/list2.py
@@ -0,0 +1,70 @@
+'''
+# set
+s1 = {1, 1, 1, 1, 1, 1, 1, 1, 2}
+print(s1)
+s2 = ['안녕', '잘가', 'Hi', 'Hi']
+print(s2)
+print(set(s2))
+'''
+'''
+s1 = {1, 2, 3, 3, 4}
+print(s1)
+s1.add(5)
+print(s1)
+s1.update([6, 7, 8, 9, 10])
+print(s1)
+s1.remove(3)  # 없는 내용을 삭제시 오류 출력
+print(s1)
+s1.discard(9)  # 없는 내용을 삭제하여도 오류 값을 출력하지 않음
+print(s1)
+s1.clear()
+print(s1)
+'''
+'''
+s1 = {1, 2, 3, 4, 5}
+s2 = {4, 5, 6, 7, 8}
+# 합집합
+s3 = s1 | s2
+print(s3)
+s3 = s1.union(s2)
+print(s3)
+
+# 교집합
+s3 = s1 & s2
+print(s3)
+s3 = s1.intersection(s2)
+print(s3)
+
+# 차집합
+s3 = s1 - s2
+print(s3)
+s3 = s1.difference(s2)
+print(s3)
+'''
+
+# dictionary
+# 생성
+dict1 = {}
+dict1 = dict()
+print(dict1)
+dict1 = {
+    "name": "김준식",
+    "age": 25,
+    "city": "서울",
+    "hobby": ["런닝", "등산", "헬스"]
+}
+print(dict1)
+dict2 = dict(name="김준식", age=25)
+print(dict2)
+print(dict1['name'])
+print(dict1['hobby'])
+
+# 값 수정
+dict1["age"] = 21
+print(dict1)
+# 값 추가
+dict1["birthday"] = 20001011
+dict1['hobby'] = ['런닝', '등산', '헬스', "캠핑"]
+print(dict1)
+del dict1['birthday']
+print(dict1)

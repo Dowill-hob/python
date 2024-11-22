3675eb53730d7536a4387e023468bb6e5dba22da
Dowill-hob <wiewk12365@gmail.com>
2024-11-22 15:53:48 +0900

    1122

--- 1122/list2.py
+++ 1122/list2.py
@@ -41,7 +41,7 @@ print(s3)
 s3 = s1.difference(s2)
 print(s3)
 '''
-
+'''
 # dictionary
 # 생성
 dict1 = {}
@@ -68,3 +68,58 @@ dict1['hobby'] = ['런닝', '등산', '헬스', "캠핑"]
 print(dict1)
 del dict1['birthday']
 print(dict1)
+'''
+'''
+# 딕셔너리 메서드
+fruits = {
+    'apple': '사과',
+    'banana': '바나나'
+}
+print(fruits.get('apple'))
+print(fruits.get('peach'))
+print(fruits.get('peach', '복숭아'))
+
+# 여러요소 추가
+fruits.update({
+    'grapes': '포도',
+    'melon': '멜론'
+})
+print(fruits)
+print(fruits.keys())
+print(fruits.values())
+print(fruits.items())
+fruits.clear()
+print(fruits)
+'''
+'''
+# 실습.딕셔너리
+# 1
+dict1 = {}
+# 2
+dict1 ={   
+    'Alice': 85,
+    'Bob': 90,
+    'Charlie': 90
+}
+# 3
+dict1.update({'David': 80})
+# 4
+dict1['Alice'] = 88
+# 5
+del dict1['Bob']
+'''
+
+# 내장함수
+# sum()
+'''
+numbers = [1, 2, 3, 4, 5]
+print(sum(numbers))
+
+scores = {"국어": 90, "영어": 80, "수학": 85}
+print(sum(scores.values()))
+'''
+# zip()
+names = ['Alice', 'Bob', 'Charlie', 'David']
+scores = [85, 90, 88, 95]
+zipped = list(zip(names, scores))
+print(zipped)
